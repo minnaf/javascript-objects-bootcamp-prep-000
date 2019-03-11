ad106d69b87856f519e2bbc5a3357a1c285606bb
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-11 22:29:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {thirdEyeBlind: 'semiCharmedLife' }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = 'songTitle';
+  playlist.artistName = 'songTitle';
   return playlist
   
 }
